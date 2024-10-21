17091a26e4e9f53ff3b1b1fe2bd75f8ce6e3ffe4
RMxitaryan <romanmxitaryan12@gmail.com>
2024-10-21 17:48:37 +0300

    ch2

--- examples.cpp
+++ examples.cpp
@@ -2,7 +2,44 @@
 
 int main()
 {
-    std::cout << "Hello";
+    // Task1
+    /*int num;
+    std::cout << "Your number is ";
+    std::cin >> num;
+    std::cout << "The last digit is " << num % 10;*/
+
+    //Task2
+    /*int num1, num2;
+    std::cout << "First number is ";
+    std::cin >> num1;
+    std::cout << "Second number is ";
+    std::cin >> num2;
+    if (num1 + num2 > 20) {
+        std::cout << num1 * num2;
+    }
+    else if (num1 >= num2) {
+        std::cout << num1 / num2 << " mnacord " << num1 % num2;
+    }
+    else {
+        std::cout << num2 / num1 << " mnacord " << num2 % num1;
+    }*/
+
+    //Task3
+    /*double num1, num2, num3;
+    std::cout << "First number is ";
+    std::cin >> num1;
+    std::cout << "Second number is ";
+    std::cin >> num2;
+    std::cout << "Third number is ";
+    std::cin >> num3;
+    if ((num1 + num3) / 2 == num2) {
+        std::cout << "Common difference is " << num2 - num1;
+    }
+    else {
+        std::cout << "This numbers are not formed an arithmetical progression";
+    }*/
+
+    return 0;
 }
 
 
