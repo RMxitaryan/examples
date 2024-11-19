3fe9d3daeace1dccc1dccba8e53d633df9ac848b
RMxitaryan <romanmxitaryan12@gmail.com>
2024-11-19 16:52:37 +0300

    Brackets Balance

--- examples.cpp
+++ examples.cpp
@@ -2,40 +2,27 @@
 
 int main() {
 
-	//6. Find Duplicate Elements: Write a program to identify and count duplicate elements in an array.
-
-	int const N = 10;
-	int arr[N];
-	for (int i = 0;i < N;i++) {
-		std::cin >> arr[i];
-	}
-	int dublicate[N / 2];
-	int ind = 0;
-	for (int i = 0;i < N;i++) {
-		bool exist = false;
-		for (int a = 0;a < N / 2;a++) {
-			if (dublicate[a] == arr[i]) {
-				exist = true; 
+	char line[100];
+	std::cin >> line;
+	int i = 0;
+	int count = 0;
+	while (line[i] != '\0') {
+		if (line[i] == '(') {
+			count++;
+		}
+		else if (line[i] == ')') {
+			count--;
+			if (count < 0) {
 				break;
 			}
 		}
-		if (!exist) {
-          for (int j = i + 1;j < N;j++) {
-			  if (arr[j] == arr[i]) {
-				dublicate[ind] = arr[j];
-				ind++;
-				break;
-			  }
-		  }
-		}
-		
+		i++;
 	}
-	int count = 0;
-	for (int i = 0;i < ind;++i) {
-		count++;
+	if (!count) {
+		std::cout << "all is good";
+	}else {
+		std::cout << "missing a bracket";
 	}
-	std::cout << count;
-
 	
 	return 0;
 }
