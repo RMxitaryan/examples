495a74714417a6a86de63c1ac279952b85103e22
RMxitaryan <romanmxitaryan12@gmail.com>
2025-01-06 18:11:07 +0300

    zangvaci tareri entabazmutyunner

--- examples.cpp
+++ examples.cpp
@@ -1,41 +1,27 @@
 ﻿#include <iostream>
+#include <cmath>
 
 int main() {
-
-	//6. Find Duplicate Elements: Write a program to identify and count duplicate elements in an array.
-
-	int const N = 10;
-	int arr[N];
-	for (int i = 0;i < N;i++) {
-		std::cin >> arr[i];
-	}
-	int dublicate[N / 2];
-	int ind = 0;
-	for (int i = 0;i < N;i++) {
-		bool exist = false;
-		for (int a = 0;a < N / 2;a++) {
-			if (dublicate[a] == arr[i]) {
-				exist = true;
-				break;
-			}
-		}
-		if (!exist) {
-			for (int j = i + 1;j < N;j++) {
-				if (arr[j] == arr[i]) {
-					dublicate[ind] = arr[j];
-					ind++;
-					break;
-				}
-			}
-		}
-
-	}
-	int count = 0;
-	for (int i = 0;i < ind;++i) {
-		count++;
-	}
-	std::cout << count;
-
-
-	return 0;
+    int n;
+    std::cin >> n;
+    int* arr = new int[n];
+    for (int i = 0;i < n;++i) {
+        std::cin >> arr[i];
+    }
+    for (int i = 1;i < pow(2,n);++i) {
+        int* arr2 = new int[n];
+        for (int j = n - 1;j >= 0;j--) {
+            int c = (i >> j) & 1;
+            arr2[n - j - 1] = c;
+        }
+        for (int i = 0;i < n;i++) {
+            if (arr2[i]) {
+                std::cout << arr[i] << ' ';
+            }
+        }
+        std::cout << '\n';
+        delete[] arr2;
+    }
+    delete[] arr;
+    return 0;
 }
