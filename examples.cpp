d7ec72daed4113ddca50c12084d7d71ead3735c4
RMxitaryan <romanmxitaryan12@gmail.com>
2025-01-07 18:16:35 +0300

    Selection and Bubble sorts

--- examples.cpp
+++ examples.cpp
@@ -2,26 +2,39 @@
 #include <cmath>
 
 int main() {
+    const int N = 10;
+    int arr[N];
     int n;
     std::cin >> n;
-    int* arr = new int[n];
+
     for (int i = 0;i < n;++i) {
-        std::cin >> arr[i];
+        std:: cin >> arr[i];
     }
-    for (int i = 1;i < pow(2,n);++i) {
-        int* arr2 = new int[n];
-        for (int j = n - 1;j >= 0;j--) {
-            int c = (i >> j) & 1;
-            arr2[n - j - 1] = c;
-        }
-        for (int i = 0;i < n;i++) {
-            if (arr2[i]) {
-                std::cout << arr[i] << ' ';
+    //poqraguyn araj berel (Selection Sort)
+    for (int i = 0; i < n - 1;++i) {
+        int ind = i;
+        for (int j = i + 1; j < n;++j) {
+            if (arr[j] < arr[ind]) {
+                ind = j;
             }
         }
-        std::cout << '\n';
-        delete[] arr2;
+        int elem = arr[i];
+        arr[i] = arr[ind];
+        arr[ind] = elem;
+
     }
-    delete[] arr;
+    //pxpjaki algoritm (Bubble sort)
+    for (int i = 0; i < n - 1;++i) {
+        for (int j = 0; j < n - i - 1;++j) {
+            if (arr[j] > arr[j + 1]) {
+                int elem = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = elem;
+            }
+        }
+    }
+    for (int i = 0;i < n;++i) {
+        std::cout << arr[i] << '\n';
+    }
     return 0;
 }
