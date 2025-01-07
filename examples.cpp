ee401661761e60239b8a24f11b79da033e24165c
RMxitaryan <romanmxitaryan12@gmail.com>
2025-01-07 18:29:16 +0300

    Binary search

--- examples.cpp
+++ examples.cpp
@@ -10,31 +10,24 @@ int main() {
     for (int i = 0;i < n;++i) {
         std:: cin >> arr[i];
     }
-    //poqraguyn araj berel (Selection Sort)
-    for (int i = 0; i < n - 1;++i) {
-        int ind = i;
-        for (int j = i + 1; j < n;++j) {
-            if (arr[j] < arr[ind]) {
-                ind = j;
-            }
-        }
-        int elem = arr[i];
-        arr[i] = arr[ind];
-        arr[ind] = elem;
+    int key;
+    std::cin >> key;
+    int left = 0, right = n - 1;
 
-    }
-    //pxpjaki algoritm (Bubble sort)
-    for (int i = 0; i < n - 1;++i) {
-        for (int j = 0; j < n - i - 1;++j) {
-            if (arr[j] > arr[j + 1]) {
-                int elem = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = elem;
-            }
+    while (left <= right) {
+        int mid = (left + right)/ 2;
+
+        if (arr[mid] == key) {
+            std::cout << mid;
+            break;
+        }
+        else if (arr[mid] < key) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
         }
     }
-    for (int i = 0;i < n;++i) {
-        std::cout << arr[i] << '\n';
-    }
+   
     return 0;
 }
