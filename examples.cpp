8d625772250e06eea1214cf3c44b40058262cb4a
RMxitaryan <romanmxitaryan12@gmail.com>
2025-01-12 17:12:29 +0300

    2 hajord. 1 mec hajord

--- examples.cpp
+++ examples.cpp
@@ -1,38 +1,53 @@
 ﻿#include <iostream>
 
-bool simetrik(int num) {
-    int arr[10] = {};
-    int digitCount = 0;
-    while (num) {
-        digitCount++;
-        arr[num % 10]++;
-        num /= 10;
-    }
-    int a = 0;
-    if (arr[0] == digitCount - 1) {
-        return false;
-    }
-    for (int i = 0;i < 10;++i) {
-        if (arr[i] % 2 == 1) {
-            ++a;
-        }
-    }
-    if (a <= 1) {
-        return true;
-    }
-    return false;
-}
 
 int main() {
-    int a, b;
-    std::cin >> a >> b;
-    int count = 0;
-    for (int i = a;i <= b;++i) {
-        if (simetrik(i)) {
-            ++count;
+    int N[20] = { 55,55,56,57,80,81,82,100,101,102,103,105,105,105,105,106,106,106,106,106 };
+    int M[30] = { 70,69,68,67,66,65,64,63,62,61,60,59,58,57,54,54,54,53,52,51,50,49,48,47,46,46,46,45,44,43};
+    int C[50];
+    int i = 0;
+    int j = 29;
+    int k = 0;
+    while (k < 50) {
+        if (i == 20 && j == -1) {
+            break;
         }
+        if (k >= 1) {
+          if (C[k - 1] == N[i]) {
+            ++i;
+            continue;
+          }
+          if (C[k - 1] == M[j]) {
+            --j;
+            continue;
+          }
+        }
+        
+        if (i == 20) {
+            C[k] = M[j];
+            --j;
+            ++k;
+            continue;
+        }
+        if (j == -1) {
+            C[k] = N[i];
+            ++i;
+            ++k;
+            continue;
+        }
+        if (N[i] <= M[j]) {
+            C[k] = N[i];
+            ++i;
+        }
+        else {
+            C[k] = M[j];
+            --j;
+        }
+        ++k;
     }
-    std::cout << count;
+    for (int t = 0;t < k;++t) {
+        std::cout << C[t] << '\n';
+    }
     
   
     return 0;
